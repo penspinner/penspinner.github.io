447a39e34fd76ec6e1231d0712e187b085f9be7d
Steven Liao <Stvnliao@yahoo.com>
2019-01-01 13:36:19 -0500

    Set up testing environment

---
+++ src/components/DeskImage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+import deskImageSrc from '../images/desk.jpg';
+
+const DeskImage = () => <Image fluid src={deskImageSrc} />;
+
+export default DeskImage;
